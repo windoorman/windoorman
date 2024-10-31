2054d530394b10babf0eb7c490f225de4c51167a
하정수 <kmk2528@naver.com>
2024-10-31 11:01:56 +0900

    feat: 창문페이지 구현 중

--- FE/dev-dist/sw.js
+++ FE/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5199072c'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.n9aikednod8"
+    "revision": "0.au6m9m9hgpg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- FE/src/pages/WindowPage/WindowPage.tsx
+++ FE/src/pages/WindowPage/WindowPage.tsx
@@ -1,7 +1,33 @@
+import windowWind from "../../assets/window/창문환기.png";
+import sadGhost from "../../assets/window/mynaui_sad-ghost-solid.png";
+
 const WindowPage = () => {
   return (
     <div>
-      <h1>Window Page</h1>
+      <div>
+        <h1>Window Page</h1>
+      </div>
+      <div className="mx-8 ">
+        <img src={windowWind} alt="창문 환기" />
+      </div>
+      <div className="mt-8 pt-2 border-t-2 rounded-3xl">
+        <div className="px-28">
+          <div className=" mb-4 mt-16">
+            <img src={sadGhost} alt="슬픈 유령" />
+          </div>
+          <div className="text-[#3C4973] text-2xl font-semibold mb-4">
+            <h2>아직 등록된</h2>
+            <h2>집이 없어요!</h2>
+          </div>
+          <div>
+            <button className="bg-[#3C4973] rounded-full w-full py-1">
+              <span className="text-white text-sm font-semibold">
+                집 등록하기
+              </span>
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
