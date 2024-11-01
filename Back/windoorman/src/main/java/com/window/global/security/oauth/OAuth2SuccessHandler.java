eaa2b4ac1bf8ab622d4e2dc0f99c3e9206e5a51c
홍경현 <hyeon21owo@gmail.com>
2024-11-01 16:55:53 +0900

    feature: refresh token cookie에 저장

--- Back/windoorman/src/main/java/com/window/global/security/oauth/OAuth2SuccessHandler.java
+++ Back/windoorman/src/main/java/com/window/global/security/oauth/OAuth2SuccessHandler.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import jakarta.servlet.http.Cookie;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -32,13 +33,23 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
         log.info("accessToken: {} refreshToken: {}", accessToken, refreshToken);
 
         String redirectUrl = UriComponentsBuilder.
-                fromUriString("http://k11b107a.p.ssafy.io/token")
+                fromUriString("https://k11b107a.p.ssafy.io/token")
                 .queryParam("access", accessToken)
-                .queryParam("refresh", refreshToken)
                 .build()
                 .toString();
 
+        response.addCookie(createCookie("refresh", refreshToken));
         response.setContentType("application/json");
         response.sendRedirect(redirectUrl);
+    }
+
+    private Cookie createCookie(String key, String value) {
+        Cookie cookie = new Cookie(key, value);
+        cookie.setMaxAge(60*60*60);
+        //cookie.setSecure(true);
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+
+        return cookie;
     }
 }
