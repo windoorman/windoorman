3cd4c83d9a4b850f5b7b4856fcab39293b8f2326
홍경현 <hyeon21owo@gmail.com>
2024-11-08 11:10:45 +0900

    feature: 창문 개폐 로그 조회 service

--- Back/windoorman/src/main/java/com/window/domain/report/service/ReportService.java
+++ Back/windoorman/src/main/java/com/window/domain/report/service/ReportService.java
@@ -2,9 +2,12 @@ package com.window.domain.report.service;
 
 import com.window.domain.member.entity.Member;
 import com.window.domain.place.repository.PlaceRepository;
+import com.window.domain.report.dto.ActionsReportResponseDto;
 import com.window.domain.report.dto.AirReportResponseDto;
 import com.window.domain.report.entity.Report;
 import com.window.domain.report.repository.ReportRepository;
+import com.window.domain.windowAction.entity.WindowAction;
+import com.window.domain.windowAction.repository.WindowActionRepository;
 import com.window.global.exception.CustomException;
 import com.window.global.exception.ExceptionResponse;
 import com.window.global.util.MemberInfo;
@@ -15,6 +18,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -24,9 +29,11 @@ public class ReportService {
 
     private final ReportRepository reportRepository;
     private final PlaceRepository placeRepository;
+    private final WindowActionRepository windowActionRepository;
 
     public AirReportResponseDto findAirReport(Long placeId, LocalDate reportDate, Authentication authentication) {
         Member member = MemberInfo.getMemberInfo(authentication);
+        log.info("member: {}", member);
         if(!placeRepository.existsByIdAndMember(placeId, member)){
             throw new ExceptionResponse(CustomException.NOT_FOUND_PLACE_EXCEPTION);
         }
@@ -41,5 +48,22 @@ public class ReportService {
                 .humidity(report.getHumidity())
                 .airCondition(report.getAirCondition())
                 .build();
+    }
+
+    public List<ActionsReportResponseDto> findActionsReport(Long windowId, Authentication authentication) {
+        Member member = MemberInfo.getMemberInfo(authentication);
+
+        List<WindowAction> windowActions = windowActionRepository.findByWindowsId(windowId)
+                .orElseThrow(()->new ExceptionResponse(CustomException.NOT_FOUND_REPORT_EXCEPTION));
+
+        List<ActionsReportResponseDto> responses = new ArrayList<>();
+        for(WindowAction windowAction : windowActions){
+            responses.add(ActionsReportResponseDto.builder()
+                    .actionReportId(windowAction.getId())
+                    .open(windowAction.getOpen().toString())
+                    .openTime(windowAction.getOpenTime())
+                    .build());
+        }
+        return responses;
     }
 }
