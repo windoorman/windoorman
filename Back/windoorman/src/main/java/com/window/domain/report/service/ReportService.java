da2835b2c3a669b14488241fe10bce9b784d6f1f
홍경현 <hyeon21owo@gmail.com>
2024-11-08 13:19:11 +0900

    feature: report Service 수정

--- Back/windoorman/src/main/java/com/window/domain/report/service/ReportService.java
+++ Back/windoorman/src/main/java/com/window/domain/report/service/ReportService.java
@@ -1,13 +1,18 @@
 package com.window.domain.report.service;
 
 import com.window.domain.member.entity.Member;
+import com.window.domain.place.entity.Place;
 import com.window.domain.place.repository.PlaceRepository;
 import com.window.domain.report.dto.ActionsReportResponseDto;
-import com.window.domain.report.dto.AirReportResponseDto;
+import com.window.domain.report.dto.AirReportDto;
+import com.window.domain.report.dto.ReportResponseDto;
+import com.window.domain.report.dto.WindowsDto;
 import com.window.domain.report.entity.Report;
 import com.window.domain.report.repository.ReportRepository;
 import com.window.domain.windowAction.entity.WindowAction;
 import com.window.domain.windowAction.repository.WindowActionRepository;
+import com.window.domain.windows.entity.Windows;
+import com.window.domain.windows.model.repository.WindowsRepository;
 import com.window.global.exception.CustomException;
 import com.window.global.exception.ExceptionResponse;
 import com.window.global.util.MemberInfo;
@@ -18,6 +23,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,30 +36,62 @@ public class ReportService {
     private final ReportRepository reportRepository;
     private final PlaceRepository placeRepository;
     private final WindowActionRepository windowActionRepository;
+    private final WindowsRepository windowsRepository;
 
-    public AirReportResponseDto findAirReport(Long placeId, LocalDate reportDate, Authentication authentication) {
+    public ReportResponseDto findAirReport(Long placeId, LocalDate reportDate, Authentication authentication) {
         Member member = MemberInfo.getMemberInfo(authentication);
         log.info("member: {}", member);
-        if(!placeRepository.existsByIdAndMember(placeId, member)){
-            throw new ExceptionResponse(CustomException.NOT_FOUND_PLACE_EXCEPTION);
-        }
+
+        Place place = placeRepository.findByIdAndMember(placeId, member)
+                .orElseThrow(()-> new ExceptionResponse(CustomException.NOT_FOUND_PLACE_EXCEPTION));
 
         Report report = reportRepository.findByPlaceIdAndReportDate(placeId, reportDate)
                 .orElseThrow(()-> new ExceptionResponse(CustomException.NOT_FOUND_REPORT_EXCEPTION));
 
-        return AirReportResponseDto.builder()
+        AirReportDto airReport = AirReportDto.builder()
                 .reportId(report.getId())
                 .lowTemperature(report.getLowTemperature())
                 .highTemperature(report.getHighTemperature())
                 .humidity(report.getHumidity())
                 .airCondition(report.getAirCondition())
                 .build();
+
+        List<WindowsDto> windowsDto = findWindows(placeId);
+
+        Long windowId = windowsDto.get(0).windowsId();
+        List<ActionsReportResponseDto> actionsReport = findWindowActions(windowId, reportDate);
+
+        return ReportResponseDto.builder()
+                .placeName(place.getName())
+                .airReport(airReport)
+                .windows(windowsDto)
+                .actionsReport(actionsReport)
+                .build();
     }
 
-    public List<ActionsReportResponseDto> findActionsReport(Long windowId, Authentication authentication) {
+    public List<ActionsReportResponseDto> findActionsReport(Long windowId, LocalDate reportDate, Authentication authentication) {
         Member member = MemberInfo.getMemberInfo(authentication);
+        return findWindowActions(windowId, reportDate);
+    }
 
-        List<WindowAction> windowActions = windowActionRepository.findByWindowsId(windowId)
+    public List<WindowsDto> findWindows(Long placeId){
+        List<Windows> windows = windowsRepository.findAllByPlaceId(placeId)
+                .orElseThrow(() -> new ExceptionResponse(CustomException.NOT_FOUND_WINDOWS_EXCEPTION));
+
+        List<WindowsDto> responses = new ArrayList<>();
+        for(Windows window : windows){
+            responses.add(WindowsDto.builder()
+                            .windowsId(window.getId())
+                            .name(window.getName())
+                    .build());
+        }
+        return responses;
+    }
+
+    public List<ActionsReportResponseDto> findWindowActions(Long windowId, LocalDate reportDate) {
+        LocalDateTime startOfDay = reportDate.atStartOfDay();
+        LocalDateTime endOfDay = startOfDay.plusDays(1);
+        List<WindowAction> windowActions = windowActionRepository.findByWindowsIdAndDate(windowId, startOfDay, endOfDay)
                 .orElseThrow(()->new ExceptionResponse(CustomException.NOT_FOUND_REPORT_EXCEPTION));
 
         List<ActionsReportResponseDto> responses = new ArrayList<>();
