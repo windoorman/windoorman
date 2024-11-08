7093b8e44e9feeda4e3a98525580e833d6e11036
홍경현 <hyeon21owo@gmail.com>
2024-11-08 11:10:57 +0900

    feature: 창문 개폐 로그 조회 controller

--- Back/windoorman/src/main/java/com/window/domain/report/controller/ReportController.java
+++ Back/windoorman/src/main/java/com/window/domain/report/controller/ReportController.java
@@ -1,5 +1,6 @@
 package com.window.domain.report.controller;
 
+import com.window.domain.report.dto.ActionsReportResponseDto;
 import com.window.domain.report.dto.AirReportResponseDto;
 import com.window.domain.report.service.ReportService;
 import lombok.RequiredArgsConstructor;
@@ -12,18 +13,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @RestController
-@RequestMapping("/report")
+@RequestMapping("/reports")
 @RequiredArgsConstructor
 @Slf4j
 public class ReportController {
 
     private final ReportService reportService;
 
-    @GetMapping("/air/{placeId}/{windowId}")
-    public ResponseEntity<?> findAirReport(@PathVariable Long placeId, @PathVariable LocalDate windowId, Authentication authentication) {
-        AirReportResponseDto response = reportService.findAirReport(placeId, windowId, authentication);
+    @GetMapping("/air/{placeId}/{reportDate}")
+    public ResponseEntity<?> findAirReport(@PathVariable Long placeId, @PathVariable LocalDate reportDate, Authentication authentication) {
+        AirReportResponseDto response = reportService.findAirReport(placeId, reportDate, authentication);
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/actions/{windowId}")
+    public ResponseEntity<?> findActionReport(@PathVariable Long windowId, Authentication authentication) {
+        List<ActionsReportResponseDto> responses = reportService.findActionsReport(windowId, authentication);
+        return ResponseEntity.ok(responses);
     }
 }
