fe5086950aefa703fdafca77a8e8c1c4d4da1e76
홍경현 <hyeon21owo@gmail.com>
2024-11-08 13:18:58 +0900

    feature: windowsId와 해당 날짜에 해당하는 windowAction 찾는 query

--- Back/windoorman/src/main/java/com/window/domain/windowAction/repository/WindowActionRepository.java
+++ Back/windoorman/src/main/java/com/window/domain/windowAction/repository/WindowActionRepository.java
@@ -1,12 +1,19 @@
 package com.window.domain.windowAction.repository;
 
 import com.window.domain.windowAction.entity.WindowAction;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 public interface WindowActionRepository extends JpaRepository<WindowAction, Long> {
 
-    Optional<List<WindowAction>> findByWindowsId(Long windowsId);
+    @Query("SELECT wa FROM WindowAction wa WHERE wa.windows.id = :windowsId AND wa.openTime >= :startOfDay AND wa.openTime < :endOfDay")
+    Optional<List<WindowAction>> findByWindowsIdAndDate(
+            @Param("windowsId") Long windowsId,
+            @Param("startOfDay") LocalDateTime startOfDay,
+            @Param("endOfDay") LocalDateTime endOfDay);
 }
