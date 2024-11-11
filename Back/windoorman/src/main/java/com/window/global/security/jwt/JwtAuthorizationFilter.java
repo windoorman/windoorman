47a6c3e2846f8f5c9d025d2f19ced31448c0d907
이재민 <jml6534@naver.com>
2024-11-11 11:15:40 +0900

    feat: 추가 window action 파이썬 연결을 위한 라즈베리파이 whitelist

--- Back/windoorman/src/main/java/com/window/global/security/jwt/JwtAuthorizationFilter.java
+++ Back/windoorman/src/main/java/com/window/global/security/jwt/JwtAuthorizationFilter.java
@@ -1,13 +1,17 @@
 package com.window.global.security.jwt;
 
+import com.window.domain.whitelist.repository.WhitelistRepository;
 import com.window.global.exception.CustomException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -16,14 +20,30 @@ import java.io.IOException;
 
 @AllArgsConstructor
 @Slf4j
+@RequiredArgsConstructor
 public class JwtAuthorizationFilter extends OncePerRequestFilter {
 
     private JwtTokenProvider jwtTokenProvider;
+    private final WhitelistRepository whitelistRepository;
+
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String requestURI = request.getRequestURI();
         log.info("requestURI: {}", requestURI);
+
+        // 라즈베리파이 whitelist 인증
+        String macAddr = request.getHeader("mac");
+        log.info("macAddr: {}", macAddr);
+        if(macAddr != null){
+            if(whitelistRepository.existsByMacAddress(macAddr)){
+                UsernamePasswordAuthenticationToken authToken =
+                        new UsernamePasswordAuthenticationToken("macAddr", null, AuthorityUtils.createAuthorityList("ROLE_WHITELIST"));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+                filterChain.doFilter(request, response);
+                return;
+            }
+        }
 
         String accessToken = getAccessToken(request);
         if(accessToken != null && jwtTokenProvider.validateAccessToken(accessToken, request)) {
