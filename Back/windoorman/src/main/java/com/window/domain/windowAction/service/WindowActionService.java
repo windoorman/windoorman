6a9ef8b365095c1889a5380dc92035171b57ecc1
홍경현 <hyeon21owo@gmail.com>
2024-11-11 13:30:43 +0900

    feature:  평균 개폐 횟수 service

--- Back/windoorman/src/main/java/com/window/domain/windowAction/service/WindowActionService.java
+++ Back/windoorman/src/main/java/com/window/domain/windowAction/service/WindowActionService.java
@@ -1,5 +1,7 @@
 package com.window.domain.windowAction.service;
 
+import com.window.domain.windowAction.dto.response.AvgActionResponseDto;
+import com.window.domain.windowAction.dto.CountActionDto;
 import com.window.domain.windowAction.dto.request.WindowActionRequestDto;
 import com.window.domain.windowAction.entity.WindowAction;
 import com.window.domain.windowAction.repository.WindowActionRepository;
@@ -9,14 +11,17 @@ import com.window.global.exception.CustomException;
 import com.window.global.exception.ExceptionResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class WindowActionService {
 
     private final WindowActionRepository windowActionRepository;
     private final WindowsRepository windowsRepository;
 
+    @Transactional
     public Long registerWindowAction(WindowActionRequestDto dto) {
 
         Windows windows = windowsRepository.findById(dto.getWindowsId())
@@ -30,5 +35,13 @@ public class WindowActionService {
                 .build();
 
         return windowActionRepository.save(windowAction).getId();
+    }
+
+    public AvgActionResponseDto findCountAction(Long placeId) {
+        CountActionDto countActions = windowActionRepository.findByCountAction(placeId)
+                .orElseThrow();
+        return AvgActionResponseDto.builder()
+                .avgActions((double) countActions.getOpenCount()/countActions.getWindowsCount())
+                .build();
     }
 }
