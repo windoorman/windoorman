47bec71db105ab58268d9b4335543ac32b206e77
이재민 <jml6534@naver.com>
2024-11-13 20:46:35 +0900

    fix: 코드 정렬 수정

--- Back/windoorman/src/main/java/com/window/domain/report/service/ReportService.java
+++ Back/windoorman/src/main/java/com/window/domain/report/service/ReportService.java
@@ -36,70 +36,70 @@ import java.util.Map;
 @Slf4j
 public class ReportService {
 
-        private final ReportRepository reportRepository;
-        private final PlaceRepository placeRepository;
-        private final WindowActionRepository windowActionRepository;
-        private final WindowsRepository windowsRepository;
-        private final ElasticsearchOperations operations;
+    private final ReportRepository reportRepository;
+    private final PlaceRepository placeRepository;
+    private final WindowActionRepository windowActionRepository;
+    private final WindowsRepository windowsRepository;
+    private final ElasticsearchOperations operations;
 
-        public ReportResponseDto findAirReport(Long placeId, LocalDate reportDate) {
-            Report report = reportRepository.findByPlaceIdAndReportDate(placeId, reportDate)
-                    .orElseThrow(()-> new ExceptionResponse(CustomException.NOT_FOUND_REPORT_EXCEPTION));
+    public ReportResponseDto findAirReport(Long placeId, LocalDate reportDate) {
+        Report report = reportRepository.findByPlaceIdAndReportDate(placeId, reportDate)
+                .orElseThrow(() -> new ExceptionResponse(CustomException.NOT_FOUND_REPORT_EXCEPTION));
 
-            AirReportDto airReport = AirReportDto.builder()
-                    .reportId(report.getId())
-                    .lowTemperature(report.getLowTemperature())
-                    .highTemperature(report.getHighTemperature())
-                    .humidity(report.getHumidity())
-                    .airCondition(report.getAirCondition())
-                    .build();
+        AirReportDto airReport = AirReportDto.builder()
+                .reportId(report.getId())
+                .lowTemperature(report.getLowTemperature())
+                .highTemperature(report.getHighTemperature())
+                .humidity(report.getHumidity())
+                .airCondition(report.getAirCondition())
+                .build();
 
-            List<WindowsDto> windowsDto = findWindows(placeId);
+        List<WindowsDto> windowsDto = findWindows(placeId);
 
-            Long windowId = windowsDto.get(0).windowsId();
-            List<ActionsReportResponseDto> actionsReport = findWindowActions(windowId, reportDate);
+        Long windowId = windowsDto.get(0).windowsId();
+        List<ActionsReportResponseDto> actionsReport = findWindowActions(windowId, reportDate);
 
-            return ReportResponseDto.builder()
-                    .airReport(airReport)
-                    .windows(windowsDto)
-                    .actionsReport(actionsReport)
-                    .build();
+        return ReportResponseDto.builder()
+                .airReport(airReport)
+                .windows(windowsDto)
+                .actionsReport(actionsReport)
+                .build();
+    }
+
+    public List<ActionsReportResponseDto> findActionsReport(Long windowId, LocalDate reportDate) {
+        return findWindowActions(windowId, reportDate);
+    }
+
+    public List<WindowsDto> findWindows(Long placeId) {
+        List<Windows> windows = windowsRepository.findAllByPlaceId(placeId)
+                .orElseThrow(() -> new ExceptionResponse(CustomException.NOT_FOUND_WINDOWS_EXCEPTION));
+
+        List<WindowsDto> responses = new ArrayList<>();
+        for (Windows window : windows) {
+            responses.add(WindowsDto.builder()
+                    .windowsId(window.getId())
+                    .name(window.getName())
+                    .build());
         }
+        return responses;
+    }
 
-        public List<ActionsReportResponseDto> findActionsReport(Long windowId, LocalDate reportDate) {
-            return findWindowActions(windowId, reportDate);
+    public List<ActionsReportResponseDto> findWindowActions(Long windowId, LocalDate reportDate) {
+        LocalDateTime startOfDay = reportDate.atStartOfDay();
+        LocalDateTime endOfDay = startOfDay.plusDays(1);
+        List<WindowAction> windowActions = windowActionRepository.findByWindowsIdAndDate(windowId, startOfDay, endOfDay)
+                .orElseThrow(() -> new ExceptionResponse(CustomException.NOT_FOUND_REPORT_EXCEPTION));
+
+        List<ActionsReportResponseDto> responses = new ArrayList<>();
+        for (WindowAction windowAction : windowActions) {
+            responses.add(ActionsReportResponseDto.builder()
+                    .actionReportId(windowAction.getId())
+                    .open(windowAction.getOpen().toString())
+                    .openTime(windowAction.getOpenTime())
+                    .build());
         }
-
-        public List<WindowsDto> findWindows(Long placeId){
-            List<Windows> windows = windowsRepository.findAllByPlaceId(placeId)
-                    .orElseThrow(() -> new ExceptionResponse(CustomException.NOT_FOUND_WINDOWS_EXCEPTION));
-
-            List<WindowsDto> responses = new ArrayList<>();
-            for(Windows window : windows){
-                responses.add(WindowsDto.builder()
-                                .windowsId(window.getId())
-                                .name(window.getName())
-                        .build());
-            }
-            return responses;
-        }
-
-        public List<ActionsReportResponseDto> findWindowActions(Long windowId, LocalDate reportDate) {
-            LocalDateTime startOfDay = reportDate.atStartOfDay();
-            LocalDateTime endOfDay = startOfDay.plusDays(1);
-            List<WindowAction> windowActions = windowActionRepository.findByWindowsIdAndDate(windowId, startOfDay, endOfDay)
-                    .orElseThrow(()->new ExceptionResponse(CustomException.NOT_FOUND_REPORT_EXCEPTION));
-
-            List<ActionsReportResponseDto> responses = new ArrayList<>();
-            for(WindowAction windowAction : windowActions){
-                responses.add(ActionsReportResponseDto.builder()
-                        .actionReportId(windowAction.getId())
-                        .open(windowAction.getOpen().toString())
-                        .openTime(windowAction.getOpenTime())
-                        .build());
-            }
-            return responses;
-        }
+        return responses;
+    }
 
     public Map<String, Object> getLogs(Long actionId) {
         WindowAction action = windowActionRepository.findById(actionId)
@@ -114,9 +114,9 @@ public class ReportService {
         boolean isPreviousDate = !openTime.toLocalDate().equals(openTime.minusHours(1).toLocalDate());
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
 
-        String formattedDate  = openTime.format(formatter);
+        String formattedDate = openTime.format(formatter);
         String prevFormattedDate = null;
-        if(isPreviousDate)
+        if (isPreviousDate)
             prevFormattedDate = openTime.minusDays(1).toLocalDate().format(formatter);
 
         openTime = openTime.minusHours(9);
@@ -124,7 +124,7 @@ public class ReportService {
         // 엘라스틱 서치 조회
         String indexName = windows.getId() + "-" + windows.getPlace().getId() + "-" + formattedDate;
         String preIndexName = windows.getId() + "-" + windows.getPlace().getId() + "-" + prevFormattedDate;
-        if(isPreviousDate && operations.indexOps(IndexCoordinates.of(preIndexName)).exists()){
+        if (isPreviousDate && operations.indexOps(IndexCoordinates.of(preIndexName)).exists()) {
             indexName += "," + preIndexName;
         }
 
@@ -146,7 +146,7 @@ public class ReportService {
 
 
         List<GraphResponseDto> dtos = new ArrayList<>();
-        for(Graph e : elasticList) {
+        for (Graph e : elasticList) {
             GraphResponseDto dto = GraphResponseDto.builder()
                     .pm10(e.getPm10())
                     .pm25(e.getPm25())
