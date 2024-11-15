cddcf347d9c9867208f1e7c0eb11c30f4e77e8c8
이재민 <jml6534@naver.com>
2024-11-15 15:34:21 +0900

    feat: 창문 개폐 로그 로직 수정

--- Back/windoorman/src/main/java/com/window/domain/windowAction/service/WindowActionService.java
+++ Back/windoorman/src/main/java/com/window/domain/windowAction/service/WindowActionService.java
@@ -11,6 +11,8 @@ import com.window.global.exception.CustomException;
 import com.window.global.exception.ExceptionResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -27,9 +29,31 @@ public class WindowActionService {
 
     private final WindowActionRepository windowActionRepository;
     private final WindowsRepository windowsRepository;
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    @Value("${spring.redis.set.key}")
+    private String redisSetKey;
+
+    @Value("${spring.redis.action.key}")
+    private String actionKey;
 
     @Transactional
     public Long registerWindowAction(WindowActionRequestDto dto) {
+        // 스케줄에 등록되어있고
+        if(Boolean.TRUE.equals(redisTemplate.opsForSet().isMember(redisSetKey, String.valueOf(dto.getWindowsId())))){
+            // 한번 action에 등록이 됐으면
+            if(Boolean.TRUE.equals(redisTemplate.opsForSet().isMember(actionKey, String.valueOf(dto.getWindowsId())))){
+                return null;
+            }
+            // 등록이 되지 않았다면
+            redisTemplate.opsForSet().add(actionKey, String.valueOf(String.valueOf(dto.getWindowsId())));
+        }
+        // 만약에 스케줄이 등록되어있지 않은데
+        else{
+            // action에 등록이 됐다고 되어있으면
+            if(Boolean.TRUE.equals(redisTemplate.opsForSet().isMember(actionKey, String.valueOf(dto.getWindowsId()))))
+                redisTemplate.opsForSet().remove(actionKey, String.valueOf(dto.getWindowsId()));
+        }
 
         Windows windows = windowsRepository.findById(dto.getWindowsId())
                 .orElseThrow(() -> new ExceptionResponse(CustomException.NOT_FOUND_WINDOWS_EXCEPTION));
--- Back/windoorman/src/main/java/com/window/global/config/RedisConfig.java
+++ Back/windoorman/src/main/java/com/window/global/config/RedisConfig.java
@@ -1,5 +1,8 @@
 package com.window.global.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.window.domain.schedule.dto.ScheduleRedisDto;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -38,8 +41,17 @@ public class RedisConfig {
     public RedisTemplate<String, ScheduleRedisDto> scheduleRedisTemplate() {
         RedisTemplate<String, ScheduleRedisDto> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory());
+
+        // Key Serializer
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(ScheduleRedisDto.class));
+
+        // Value Serializer
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule()); // Java 8 날짜/시간 모듈 추가
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS); // ISO-8601 포맷 활성화
+        Jackson2JsonRedisSerializer<ScheduleRedisDto> valueSerializer = new Jackson2JsonRedisSerializer<>(objectMapper, ScheduleRedisDto.class);
+
+        template.setValueSerializer(valueSerializer);
 
         return template;
     }
