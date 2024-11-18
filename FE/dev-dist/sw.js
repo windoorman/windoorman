8a8ef2fe53cc3040a730a7eef326d92bc49f0014
하정수 <kmk2528@naver.com>
2024-11-18 09:16:03 +0900

    feat: 로딩 추가

--- FE/dev-dist/sw.js
+++ FE/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5199072c'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.og996p8f0o8"
+    "revision": "0.ai0pjppgvt8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- FE/src/pages/WindowPage/WindowPage.tsx
+++ FE/src/pages/WindowPage/WindowPage.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from "react";
+import SyncLoader from "react-spinners/SyncLoader"; // 로딩 스피너 임포트
 import windowWind from "../../assets/window/창문환기.png";
 import HomeMain from "../../components/home/HomeMain";
 import WindowMain from "../../components/home/WindowMain";
@@ -7,7 +8,7 @@ import useHomeStore from "../../stores/useHomeStore";
 
 const WindowPage = () => {
   const [isDropdownOpen, setDropdownOpen] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false); // 로딩 상태 관리
   const dropdownRef = useRef<HTMLDivElement>(null);
 
   const homeList = useHomeStore((state) => state.homes);
@@ -30,9 +31,9 @@ const WindowPage = () => {
   useEffect(() => {
     const fetchHomesData = async () => {
       if (homeList.length === 0) {
-        setIsLoading(true);
+        setIsLoading(true); // 로딩 시작
         await fetchHomes();
-        setIsLoading(false);
+        setIsLoading(false); // 로딩 종료
       }
     };
 
@@ -58,29 +59,34 @@ const WindowPage = () => {
 
   return (
     <div>
-      <div className="m-4 mt-10 ml-10" ref={dropdownRef}>
-        <ul
-          onClick={toggleDropdown}
-          className="text-2xl text-[#3C4973] font-bold flex items-center space-x-1"
-        >
-          {isLoading ? (
-            <div className="spinner"></div>
-          ) : (
-            <>
-              <span>{selectedHome ? selectedHome.name : "홈"}</span>
-              <span className="text-lg">{isDropdownOpen ? "▲" : "▼"}</span>
-            </>
-          )}
-        </ul>
-        {isDropdownOpen && <Dropdown onSelect={handleHomeSelect} />}
-      </div>
-      <div className="mx-8">
-        <img src={windowWind} alt="창문 환기" />
-      </div>
-      {homeList.length > 0 && selectedHome ? (
-        <WindowMain selectedHome={selectedHome} />
+      {isLoading ? (
+        // 로딩 스피너 표시
+        <div className="flex justify-center items-center h-screen">
+          <SyncLoader color="#3752A6" />
+        </div>
       ) : (
-        <HomeMain />
+        <>
+          <div className="m-4 mt-10 ml-10" ref={dropdownRef}>
+            <ul
+              onClick={toggleDropdown}
+              className="text-2xl text-[#3C4973] font-bold flex items-center space-x-1"
+            >
+              <>
+                <span>{selectedHome ? selectedHome.name : "홈"}</span>
+                <span className="text-lg">{isDropdownOpen ? "▲" : "▼"}</span>
+              </>
+            </ul>
+            {isDropdownOpen && <Dropdown onSelect={handleHomeSelect} />}
+          </div>
+          <div className="mx-8">
+            <img src={windowWind} alt="창문 환기" />
+          </div>
+          {homeList.length > 0 && selectedHome ? (
+            <WindowMain selectedHome={selectedHome} />
+          ) : (
+            <HomeMain />
+          )}
+        </>
       )}
     </div>
   );
